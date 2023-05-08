613aef0142ef8d2808ccedc662fdf5052154376c
ZAKARIA99LACHKAR <lachkar99zakaria@gmail.com>
2023-05-08 18:10:10 +0100

    Initial Commit

---
+++ popupViews/popupViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  popupViewsApp.swift
+//  popupViews
+//
+//  Created by Zakarai Lachkar on 8/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct popupViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
