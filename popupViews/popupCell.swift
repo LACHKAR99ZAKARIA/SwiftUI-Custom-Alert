d4b6be7f626812ce8e787e1a5644487b9ff0b480
ZAKARIA99LACHKAR <lachkar99zakaria@gmail.com>
2023-05-08 22:10:05 +0100

    first commit

---
+++ popupViews/popupCell.swift
@@ -0,0 +1,20 @@
+//
+//  popupCell.swift
+//  popupViews
+//
+//  Created by Zakarai Lachkar on 8/5/2023.
+//
+
+import SwiftUI
+
+struct popupCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct popupCell_Previews: PreviewProvider {
+    static var previews: some View {
+        popupCell()
+    }
+}
